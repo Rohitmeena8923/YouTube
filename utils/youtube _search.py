b488ee6091be95df2f096b7689eb8d93511cb09d
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-05-15 10:00:11 +0530

    Update youtube _search.py

--- utils/youtube _search.py
+++ utils/youtube _search.py
@@ -1,14 +1,9 @@
 from pytube import Search
-from pytube.exceptions import PytubeError
-from .helpers import clean_filename
 
 def search_youtube(query, max_results=5):
-    """Search YouTube and return results."""
     try:
-        s = Search(query)
         results = []
-        
-        for video in s.results[:max_results]:
+        for video in Search(query).results[:max_results]:
             results.append({
                 'title': video.title,
                 'url': video.watch_url,
@@ -16,7 +11,6 @@ def search_youtube(query, max_results=5):
                 'channel': video.author,
                 'thumbnail': video.thumbnail_url
             })
-        
         return results
     except Exception as e:
-        raise Exception(f"Error searching YouTube: {str(e)}")
+        raise Exception(f"Search error: {str(e)}")
